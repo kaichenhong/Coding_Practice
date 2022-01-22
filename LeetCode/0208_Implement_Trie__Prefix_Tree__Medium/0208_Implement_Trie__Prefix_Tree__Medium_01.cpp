df9016b691c577e84eb612eaf41b5f72e6249036
kaichenhong <kaichenhong@gmail.com>
2022-01-23 00:07:46 +0800

    first commit

---
+++ LeetCode/0208_Implement_Trie__Prefix_Tree__Medium/0208_Implement_Trie__Prefix_Tree__Medium_01.cpp
@@ -0,0 +1,66 @@
+/*
+ *  Results:    AC (76 ms [58.19%];  50.2 MB [11.97%])
+ */
+
+
+class Trie {
+public:
+    Trie() {
+        root = std::unique_ptr<struct TrieNode>(new TrieNode());
+    }
+
+    void insert(string word) {
+        struct TrieNode *ptr = root.get();
+
+        for (const char c : word) {
+            if (ptr->nodes[c-'a'] == nullptr)
+                ptr->nodes[c-'a'] = new TrieNode();
+            ptr = ptr->nodes[c-'a'];
+        }
+
+        ptr->eof = true;
+    }
+
+    bool search(string word) {
+        const struct TrieNode *ptr = find(word);
+
+        return (ptr && ptr->eof);
+    }
+
+    bool startsWith(string prefix) {
+        return find(prefix) != nullptr;
+    }
+private:
+    struct TrieNode {
+        TrieNode():eof(false), nodes(26, nullptr){}
+        ~TrieNode() {
+            for (struct Trieode *node : nodes)
+                if (node)
+                    delete(node);
+        }
+
+        vector<struct TrieNode*> nodes;
+        bool eof;
+    };
+
+    struct TrieNode* find(const string &word) const {
+        struct TrieNode *ptr = root.get();
+
+        for (const char c : word) {
+            ptr = ptr->nodes[c-'a'];
+            if (!ptr)
+                break;
+        }
+        return ptr;
+    }
+
+    std::unique_ptr<struct TrieNode> root;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */N
